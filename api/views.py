2f2951ce93fa2a4d3b32b7ec4dfcfea6e6dd39d4
malikouda <malikouda@gmail.com>
2019-08-06 14:35:51 -0500

    Events list now populates from databse

--- api/views.py
+++ api/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import api.controllers as controllers
+import json
 
 # Create your views here.
 def homePage(request):
-    return render(request, 'home.html')
+    events = controllers.getEvents()  
+    context = {
+        'events': events
+    }
+    return render(request, 'home.html', context)
 
 #### REQUEST HANDLERS ####
 def getUsers(request):
