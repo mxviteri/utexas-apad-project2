b9cd5b3ff46b1c46773f7f0df5ad25cd8cce7837
malikouda <malikouda@gmail.com>
2019-08-06 11:46:36 -0500

    Added base template with bootstrap and CSS hooked up

--- project2/settings.py
+++ project2/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'project2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["project2/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'project2/static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
