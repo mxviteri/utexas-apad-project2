e8debccc0403f8b0e6ee4d3fcd59f7e27bffac9e
Matt Viteri <mxviteri@gmail.com>
2019-08-18 12:23:04 -0500

    re-deploying

--- project2/urls.py
+++ project2/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 import api.views as views
 
+# deploying changes
 urlpatterns = [
     ## PAGE ROUTES ##
     path('', views.homePage),
